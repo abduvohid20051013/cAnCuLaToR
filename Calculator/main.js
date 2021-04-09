e21e961d0add259253c9915d26796750dc162979
abduvohid <74610688+abduvohid20051013@users.noreply.github.com>
2021-04-09 16:29:35 +0500

    Add files via upload

---
+++ Calculator/main.js
@@ -0,0 +1,34 @@
+function clearOutPut() {
+    $("#output").innerHTML="0"
+}
+
+function removeZero() {
+    let value =$("#output").innerHTML;
+    if(value=="0") {
+        value = " "
+        $("#output").innerHTML=value;
+    }
+}
+
+function perc() {
+    removeZero()
+    let value =$("#output").innerHTML;
+    value = value/100
+    $("#output").innerHTML=value;
+}
+
+function fordisplay(myvalue) {
+    removeZero()
+    $("#output").innerHTML+=myvalue
+}
+
+function solve() {
+    removeZero()
+    let equation =$("#output").innerHTML;
+    let solved = eval(equation)
+    $("#output").innerHTML=solved;
+}
+
+function $(e) {
+    return document.querySelector(e)
+}
